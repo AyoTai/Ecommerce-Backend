1ca37d2d9b3e089d9b4a3798cfbbe386d6886384
Tai Mujarin <tai.mujarin@gmail.com>
2021-07-06 19:19:20 -0700

    fixed tag get by id route

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -19,8 +19,8 @@ router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
   try{
-    const tagData = await Tag.findByPk({ where: {id: req.params.id},
-      include: [{model: Product, attributes: ['product_name'], through: ProductTag}]})
+    const tagData = await Tag.findByPk(req.params.id,
+      {include: [{model: Product, attributes: ['product_name'], through: ProductTag}]})
       res.status(200).json(tagData);
   } catch (err) {
     res.status(400).json(err);
