165047299d30ca29f21e53c73fa732573924b1d2
Tai Mujarin <tai.mujarin@gmail.com>
2021-07-06 18:16:37 -0700

    added category findAll and findbyPK route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,14 +3,31 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try{
+    const categoryData = await Category.findAll({
+      include: [{model: Product, attributes: ['product_name']}]
+    });
+    // console.log(categoryData);
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try{
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [{ model: Product, attributes: ['product_name']}]
+    })
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
